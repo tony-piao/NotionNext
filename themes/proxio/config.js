2c9d4c48635a45138acd9bf1faa4558d890b0cdb
朴文杰 <03398647@yto.net.cn>
2025-08-12 11:21:36 +0800

    feat(config): update config

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -2,13 +2,13 @@
  * 另一个落地页主题
  */
 const CONFIG = {
-    PROXIO_WELCOME_COVER_ENABLE: true, //是否显示页面进入的欢迎文字
+    PROXIO_WELCOME_COVER_ENABLE: false, //是否显示页面进入的欢迎文字
     PROXIO_WELCOME_TEXT: '欢迎来到此网站，点击任意位置进入', // 欢迎文字，留空则不启用
 
     // 英雄区块导航
     PROXIO_HERO_ENABLE: true, // 开启英雄区
     PROXIO_HERO_TITLE_1: `文杰's 博客`, // 英雄区文字
-    PROXIO_HERO_TITLE_2: '记录平时生活和学习', // 英雄区文字
+    PROXIO_HERO_TITLE_2: '记录生活和学习', // 英雄区文字
     // 英雄区两个按钮，如果TEXT留空则隐藏按钮
     PROXIO_HERO_BUTTON_1_TEXT: '', // 英雄区按钮
     PROXIO_HERO_BUTTON_1_URL:
